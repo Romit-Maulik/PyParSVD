949efbbeaafbe4c9c0d88fc778bee286b4aba91e
Romit Maulik <rmaulik@beboplogin3.lcrc.anl.gov>
2021-04-17 13:32:59 -0500

    Started ERA5 tutorial

---
+++ tutorials/era5/tutorial_era5.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import time
+import numpy as np
+
+# Current, parent and file paths
+CWD = os.getcwd()
+CF  = os.path.realpath(__file__)
+CFD = os.path.dirname(CF)
+
+# Import library specific modules
+sys.path.append(os.path.join("../../"))
+from pyparsvd.parsvd_serial   import ParSVD_Serial
+from pyparsvd.parsvd_parallel import ParSVD_Parallel
+
+# Construct SVD objects
+ParSVD = ParSVD_Parallel(K=10, ff=1.0, low_rank=True)
+
+# Path to data
+path = os.path.join(CFD, './data/')
+
+# Batchwise data - note these are h5 files and do not need the "np.load"
+initial_data = os.path.join(path, 'era5_0.h5') # Needs to be generated (dofs x snapshots)
+new_data = os.path.join(path, 'era5_1.h5')
+newer_data = os.path.join(path, 'era5_2.h5')
+newest_data = os.path.join(path, 'era5_3.h5')
+
+# Do first modal decomposition -- Parallel
+s = time.time()
+ParSVD.initialize(initial_data,dataset='dataset')
+
+# Incorporate new data -- Parallel
+ParSVD.incorporate_data(new_data,dataset='dataset')
+ParSVD.incorporate_data(newer_data,dataset='dataset')
+ParSVD.incorporate_data(newest_data,dataset='dataset')
+if ParSVD.rank == 0: print('Elapsed time PARALLEL: ', time.time() - s, 's.')
+
+# Basic postprocessing
+if ParSVD.rank == 0:
+
+    # Save results
+    ParSVD.save()
+
+    # Visualize singular values and modes modes
+    ParSVD.plot_singular_values(filename='parallel_sv.png')
+    ParSVD.plot_1D_modes(filename='parallel_1d_mode0.png')
+    ParSVD.plot_1D_modes(filename='parallel_1d_mode2.png', idxs=[2])
